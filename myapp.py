03706fd34df8a1ab4ef3ba0c3b1671a205fa3bd3
spol29 <68199583+spol-29@users.noreply.github.com>
2023-09-04 12:35:32 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,9 +2,9 @@ import yfinance as yf
 import streamlit as st
 
 st.write("""
-# Simple Stock Price App
+# Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Stock closing price and volume of Google.
 
 """)
 
